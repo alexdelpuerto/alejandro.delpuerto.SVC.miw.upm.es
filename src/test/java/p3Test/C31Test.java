cdfe0fe0f05e47e93c04adeaa4f2b72a2e8d1e49
Alex del Puerto <alejandro.delpuerto2@gmail.com>
2018-09-24 19:40:45 +0200

    Created C31Test

---
+++ src/test/java/p3Test/C31Test.java
@@ -0,0 +1,27 @@
+package p3Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import p2.C21;
+import p3.C31;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C31Test {
+    private C31 c31;
+
+    @BeforeEach
+    void before() {
+        this.c31 = new C31();
+    }
+
+    @Test
+    void testM1() {
+        assertEquals("m1", this.c31.m1());
+    }
+
+    @Test
+    void testM2() {
+        assertEquals("m2", this.c31.m2());
+    }
+}
